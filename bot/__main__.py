31536a97aac3e52d6567466e2d07a86062b89d7e
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-09-24 10:39:47 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | @AbirHasan2005 | @Technical-Jigar
+# Abhir hasaan is noob , pro kanger is noob :)
+"""
+Here is one tip for abhir hassan , learn ffmpeg first!
+"""
 
-
+from datetime import datetime as dt
 import os
-
+from bot.helper_funcs.utils import on_task_complete
 from bot import (
     APP_ID,
     API_HASH,
@@ -13,12 +14,11 @@ from bot import (
     LOGGER,
     TG_BOT_TOKEN,
     BOT_USERNAME,
-    SESSION_NAME
+    SESSION_NAME,
+    DATABASE_URL,
+    data
 )
-from bot.plugins.new_join_fn import (	
-    help_message_f	
-)
-
+from bot.helper_funcs.utils import add_task
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 
@@ -28,13 +28,33 @@ from bot.plugins.incoming_message_fn import (
     incoming_cancel_message_f
 )
 
+
 from bot.plugins.status_message_fn import (
+    eval_message_f,
     exec_message_f,
     upload_log_file
 )
 
 from bot.commands import Command
 from bot.plugins.call_back_button_handler import button
+sudo_users = "1391975600 888605132 1760568371"
+
+uptime = dt.now()
+
+def ts(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + "d, ") if days else "")
+        + ((str(hours) + "h, ") if hours else "")
+        + ((str(minutes) + "m, ") if minutes else "")
+        + ((str(seconds) + "s, ") if seconds else "")
+        + ((str(milliseconds) + "ms, ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
 
 if __name__ == "__main__" :
     # create download directory, if not exist
@@ -42,17 +62,12 @@ if __name__ == "__main__" :
         os.makedirs(DOWNLOAD_LOCATION)
     #
     
-    app = Client(
-        SESSION_NAME,
-        bot_token=TG_BOT_TOKEN,
-        api_id=APP_ID,
-        api_hash=API_HASH,
-        workers=2
-    )
+    
     #
     app.set_parse_mode("html")
     #
-    
+    # STATUS ADMIN Command
+
     # START command
     incoming_start_message_handler = MessageHandler(
         incoming_start_message_f,
@@ -60,33 +75,75 @@ if __name__ == "__main__" :
     )
     app.add_handler(incoming_start_message_handler)
     
-    # COMPRESS command
-    incoming_compress_message_handler = MessageHandler(
-        incoming_compress_message_f,
-        filters=filters.command(["compress", f"compress@{BOT_USERNAME}"])
-    )
-    app.add_handler(incoming_compress_message_handler)
+            
+    @app.on_message(filters.incoming & filters.command(["compress", f"compress@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        if message.chat.id not in AUTH_USERS:
+            return await message.reply_text("You are not authorised to use this bot")
+        query = await message.reply_text("```Added to Queue ⏰...```\nPlease be patient, Compress will start soon", quote=True)
+        data.append(message.reply_to_message)
+        if len(data) == 1:
+         await query.delete()   
+         await add_task(message.reply_to_message)     
+            
+                             
+    @app.on_message(filters.incoming & filters.command(["restart", f"restart@{BOT_USERNAME}"]))
+    async def restarter(app, message):
+      await message.reply_text("Rebooting ...")
+      quit(1)
     
-    # CANCEL command
-    incoming_cancel_message_handler = MessageHandler(
-        incoming_cancel_message_f,
-        filters=filters.command(["cancel", f"cancel@{BOT_USERNAME}"]) & filters.chat(chats=AUTH_USERS)
-    )
-    app.add_handler(incoming_cancel_message_handler)
-    
-    # HELP command
-    help_text_handler = MessageHandler(
-        help_message_f,
-        filters=filters.command(["help", f"help@{BOT_USERNAME}"])
-    )
-    app.add_handler(help_text_handler)
-    
-    # Telegram command to upload LOG files
-    upload_log_f_handler = MessageHandler(
-        upload_log_file,
-        filters=filters.command(["log", f"log@{BOT_USERNAME}"]) & filters.chat(chats=AUTH_USERS)
-    )
-    
+        
+    @app.on_message(filters.incoming & (filters.video | filters.document))
+    async def help_message(app, message):
+        if message.chat.id not in AUTH_USERS:
+            return await message.reply_text("You are not authorised to use this bot")
+        query = await message.reply_text("```Added to Queue ⏰...```\nPlease be patient, Compress will start soon", quote=True)
+        data.append(message)
+        if len(data) == 1:
+         await query.delete()   
+         await add_task(message)
+            
+    @app.on_message(filters.incoming & (filters.photo))
+    async def help_message(app, message):
+        if message.chat.id not in AUTH_USERS:
+            return await message.reply_text("You are not authorised to use this bot, contact @SenpaiAF for permission")
+        os.system('rm thumb.jpg')
+        await message.download(file_name='/app/thumb.jpg')
+        await message.reply_text('Thumbnail Added')
+        
+    @app.on_message(filters.incoming & filters.command(["cancel", f"cancel@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await incoming_cancel_message_f(app, message)
+        
+        
+    @app.on_message(filters.incoming & filters.command(["exec", f"exec@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await exec_message_f(app, message)
+        
+    @app.on_message(filters.incoming & filters.command(["eval", f"eval@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await eval_message_f(app, message)
+        
+    @app.on_message(filters.incoming & filters.command(["stop", f"stop@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await on_task_complete()    
+   
+    @app.on_message(filters.incoming & filters.command(["help", f"help@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await message.reply_text("I Warn U 2 Not Use this Bot ", quote=True)
+  
+    @app.on_message(filters.incoming & filters.command(["log", f"log@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await upload_log_file(app, message)
+    @app.on_message(filters.incoming & filters.command(["ping", f"ping@{BOT_USERNAME}"]))
+    async def up(app, message):
+      stt = dt.now()
+      ed = dt.now()
+      v = ts(int((ed - uptime).seconds) * 1000)
+      ms = (ed - stt).microseconds / 1000
+      p = f"🌋Pɪɴɢ = {ms}ms"
+      await message.reply_text(v + "\n" + p)
+
     call_back_button_handler = CallbackQueryHandler(
         button
     )
